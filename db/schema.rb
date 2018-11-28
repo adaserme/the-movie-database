e605b6c4a7fdc578cfb6fadcec6f3db1c3b76b60
ADAMA ADA-SERME <adamaada@oradimmo.com>
2018-11-28 15:09:16 +0100

    migr

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,6 @@ ActiveRecord::Schema.define(version: 20181127104938) do
   create_table "movies", force: :cascade do |t|
     t.string   "title"
     t.integer  "release_year"
-    t.string   "director"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
