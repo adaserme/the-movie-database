4735a50707f7100c576e3d31d5e99c55aa00bb97
ADAMA ADA-SERME <adamaada@oradimmo.com>
2018-11-28 17:14:56 +0100

    change the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,18 +11,26 @@ Director.destroy_all
 Movie.create!([{
   title: "INVISIBLE",
   release_year: "2013",
+  first_name: "ADA",
+  last_name: "SERME",
 },
 {
   title: "LA CHUTE DE LA MAISON BLANCHE",
   release_year: "2013",
+  first_name: "SERME",
+  last_name: "MOHAMED",
 },
 {title: "FAST AND FURIOUS",
  release_year: "2017",
+ first_name: "CISSE",
+ last_name: "INZA",
 
 },
 {
   title: "Terminator Genisys",
   release_year: "2012",
+  first_name: "BAH",
+  last_name: "EL HADJ",
 }])
  
 p "Created #{Movie.count} movies"
