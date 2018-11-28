c13a8a9ac4e5255fef3b21f1e4101cb082454859
ADAMA ADA-SERME <adamaada@oradimmo.com>
2018-11-28 13:30:36 +0100

    change root to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root to: 'movies#home'
+
+  root to: 'movies#index'
   
-  get 'movies/index'
+  get '/home', to: 'movies#home'
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
