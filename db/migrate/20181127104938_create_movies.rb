8c81f93455d9000eee4510c6d91f6a9ef5ab64fd
ADAMA ADA-SERME <adamaada@oradimmo.com>
2018-11-28 15:06:42 +0100

    suppression de la colone director

--- db/migrate/20181127104938_create_movies.rb
+++ db/migrate/20181127104938_create_movies.rb
@@ -3,7 +3,7 @@ class CreateMovies < ActiveRecord::Migration[5.0]
     create_table :movies do |t|
       t.string :title
       t.integer :release_year
-      t.string :director
+    
 
       t.timestamps
     end
