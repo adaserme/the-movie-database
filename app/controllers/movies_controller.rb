aa8aedac8314e74f5784057de1e5313ba5bed171
ADAMA ADA-SERME <adamaada@oradimmo.com>
2018-11-29 15:15:35 +0100

    add fonction update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,29 @@
 class MoviesController < ApplicationController
   def index
 
+    #creation de la fonction index qui permet d'afficher tous mes movies#
     @movies = Movie.all
     
   end
 
   def show
+    #creation de la fonction show qui permet de consulter mes movies#
     @movie = Movie.find(params[:id])
   end
 
   def edit
+    #creation de la fonction edit qui permet d'éditer un movie#
+    #recuperation du movie#
     @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    movie_params = params.require(:movie).permit(:title)
+
+    @movie.update(movie_params)
+    redirect_to movies_path
+
   end
   
 
